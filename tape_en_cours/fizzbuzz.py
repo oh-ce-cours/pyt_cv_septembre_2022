f1fe7cbc5f462eaf03a40658472c096f53766c3c
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-27 10:40:37 +0200

    gitautosync update 37

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -1,7 +1,7 @@
 for nombre in range(1, 101):
-    elif nombre % 5 == 0 and nombre % 3 == 0:
+    if nombre % 5 == 0 and nombre % 3 == 0:
         print("fizzbuzz")
-    if nombre % 3 == 0:
+    elif nombre % 3 == 0:
         print("fizz")
     elif nombre % 5 == 0:
         print("buzz")
