50d08c29da1b251b94d85fcab886aedf783ca28e
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-27 11:30:35 +0200

    gitautosync update 75

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -28,4 +28,5 @@ for nombre in range(1, 101):
     gauche = gauche_plus + droite_plus
     droite = str(nombre)
     print(gauche or droite)
-    # "fizz" * (nombre % 3 == 0) + "buzz" * (nombre % 5 == 0) or str(nombre)
+    # version en une ligne
+    # print("fizz" * (nombre % 3 == 0) + "buzz" * (nombre % 5 == 0) or str(nombre))
