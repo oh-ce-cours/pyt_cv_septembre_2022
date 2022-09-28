009ba8ac99b365c14782788cc71bf608371e909a
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-28 16:50:40 +0200

    gitautosync update 124

--- tape_en_cours/exemple_generation_csv.py
+++ tape_en_cours/exemple_generation_csv.py
@@ -3,7 +3,12 @@ import requests
 
 r = requests.get("https://api.github.com/events")
 
+
 def csv_generator_1(data: typing.List[typing.Tuple]):
+    with open("api1.csv", "w", encoding="utf8"):
+        print(data)
+        # for line in data:
+
 
 for event in r.json():
     print(f'{event["actor"]["display_login"]} sur le dépot {event["repo"]["name"]}')
