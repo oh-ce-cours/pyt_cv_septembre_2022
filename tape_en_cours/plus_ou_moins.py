d2d774161fbe2f3b5d931de41a6fb8edeab109f2
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-28 11:31:18 +0200

    gitautosync update 60

--- tape_en_cours/plus_ou_moins.py
+++ tape_en_cours/plus_ou_moins.py
@@ -2,11 +2,11 @@ import random
 
 
 def jeu_plus_moins():
-    a = int(input("Entrez un nombre entre 1 et 100 : "))
     try:
-        nombre = random.randint(1, 100)
+    a = int(input("Entrez un nombre entre 1 et 100 : "))
     except:
         print("on a dit un nombre")
+    nombre = random.randint(1, 100)
     print(nombre)
     while a != nombre:
         if a < nombre:
