f0186d1b2b82c0086bdd5858bc3c636b76cbdf99
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-28 10:42:31 +0200

    gitautosync update 46

--- tape_en_cours/fonctions.py
+++ tape_en_cours/fonctions.py
@@ -1,9 +1,18 @@
+print("avant def nouvelle")
+
+
 def ma_nouvelle_fonction():
+    print("dans nouvelle")
     return ma_nouvelle_fonction_autre()
 
 
+print("avant def autre nouvelle")
+
+
 def ma_nouvelle_fonction_autre():
+    print("dans autre nouvelle")
     return 3
 
 
+print("avant appel de nouvelle")
 ma_nouvelle_fonction()
