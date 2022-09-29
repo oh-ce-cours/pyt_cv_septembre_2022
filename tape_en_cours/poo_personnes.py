b138268a21ee96c939190512fd2de2aa7124173c
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-29 15:32:02 +0200

    gitautosync update 258

--- tape_en_cours/poo_personnes.py
+++ tape_en_cours/poo_personnes.py
@@ -9,7 +9,7 @@ class Personne:
         self.nom = nom
 
     def est_majeur(self):
-        return date.today().year - self.annee_naissance >= Personne.AGE_MAJORITE
+        return date.today().year - self.annee_naissance >= self.AGE_MAJORITE
 
 
 personne1 = Personne(1990, "Matthieu")
@@ -19,7 +19,7 @@ print(
 )
 
 Personne.AGE_MAJORITE = 40
-personne1.AGE_MAJORITE = 15
+personne1.AGE_MAJORITE = 15  # on a masqué la variable de classe avec un attribut
 print(
     f"Nom {personne1.nom}, date de naissance {personne1.annee_naissance}, Majeur ? {personne1.est_majeur()}"
 )
