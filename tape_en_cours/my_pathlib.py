393e63a0378f11df9343e05b317b726ffefac75a
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 12:31:35 +0200

    gitautosync update 2

--- tape_en_cours/my_pathlib.py
+++ tape_en_cours/my_pathlib.py
@@ -1,18 +1,14 @@
 class Path:
     def __init__(self, path, plateforme):
         self.path = path
-        self.plateforme = plateforme
-        if plateforme == "windows":
-            self.separateur = "\\"
-        else:
-            self.separateur = "/"
+        self.separateur = "\\"
 
     def __str__(self):
         return self.path
 
     def append_path(self, other):
         res = self.path + self.separateur + other
-        return Path(res, self.plateforme)
+        return Path(res)
 
     def __truediv__(self, other: str):
         return self.append_path(other)
@@ -26,7 +22,7 @@ class UnixPath(Path):
     pass
 
 
-p = Path(".", "windows")
+p = Path(".")
 intermediaire = p / "toto"
 print(type(intermediaire), intermediaire)  # "./toto"
 q = intermediaire / "tata.txt"
