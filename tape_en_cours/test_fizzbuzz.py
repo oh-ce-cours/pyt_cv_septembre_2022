1113e55574902e8c8ac69fbce25f544617569934
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 15:13:23 +0200

    gitautosync update 38

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -4,3 +4,10 @@ from fizzbuzz import est_divisible_par, regle_fizzbuzz
 def test_division():
     assert est_divisible_par(3, 2) == False
     assert est_divisible_par(10, 5) == True
+
+
+def test_regle_fizz_buzz():
+    assert regle_fizzbuzz(1) == "1"
+    assert regle_fizzbuzz(3) == "fizz"
+    assert regle_fizzbuzz(5) == "buzz"
+    assert regle_fizzbuzz(15) == "fizzbuzz"
