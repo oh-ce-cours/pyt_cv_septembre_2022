68e18a3de005bf88810d2684a51864fdf0e8fe47
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 12:39:13 +0200

    gitautosync update 14

--- tape_en_cours/tempCodeRunnerFile.py
+++ tape_en_cours/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-
-    def __str__(self):
-        if self.est_majeur():
-            return f"Bonjour, je suis {self.nom}, je suis né en {self.annee_naissance} et je suis majeur"
-        else:
-            
+WindowsPath
