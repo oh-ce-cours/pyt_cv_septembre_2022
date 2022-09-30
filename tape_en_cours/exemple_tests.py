6250f3ab6f1e247c227ce9f5836fc103ff0ecb61
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 14:50:12 +0200

    gitautosync update 11

--- tape_en_cours/exemple_tests.py
+++ tape_en_cours/exemple_tests.py
@@ -4,4 +4,12 @@ def addition(x, y):
 
 
 def lire_fichier(nom_fichier):
+    # pas facile à tester
+    # dépend du contenu et de l'existence du fichier
     return open(nom_fichier).readlines()
+
+
+def entre_un_nombre():
+    # pas facile à tester
+    # pour la meme entrée, plein de résultats possibles
+    return input()
