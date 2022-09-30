547a109498898fdbd6649ee5dd040e52cfa7d1d2
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 15:48:25 +0200

    gitautosync update 8

--- tape_en_cours/exemple_tkinter_button.py
+++ tape_en_cours/exemple_tkinter_button.py
@@ -7,8 +7,11 @@ root.geometry("300x200")
 root.resizable(False, False)
 root.title("Button Demo")
 
+def action_button():
+    print("bouton cliqué")
+
 # exit button
-exit_button = ttk.Button(root, text="Exit", command=lambda: root.quit())
+exit_button = ttk.Button(root, text="Cliquez moi !", command=)
 
 exit_button.pack(ipadx=5, ipady=5, expand=True)
 
