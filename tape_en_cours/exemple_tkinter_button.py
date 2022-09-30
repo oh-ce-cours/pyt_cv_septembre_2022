d5a4e4ba860f25c0d34681a0f711e37cabd22944
Matthieu Falce <falce.matthieu@gmail.com>
2022-09-30 15:52:12 +0200

    gitautosync update 11

--- tape_en_cours/exemple_tkinter_button.py
+++ tape_en_cours/exemple_tkinter_button.py
@@ -13,7 +13,7 @@ def action_button():
 
 
 # exit button
-exit_button = ttk.Button(root, text="Cliquez moi !", command=action_button)
+exit_button = ttk.Button(root, text="Cliquez moi !", command=action_button())
 
 exit_button.pack(ipadx=5, ipady=5, expand=True)
 
